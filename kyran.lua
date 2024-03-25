d354812a3aadb2c821a374d48d9aa47cb3b228fa
31KYRAN31 <108769604+31KYRAN31@users.noreply.github.com>
2024-03-25 21:54:19 +0300

    Delete kyran.lua

--- kyran.lua
+++
@@ -1,7 +0,0 @@
-API = gg.makeRequest('https://raw.githubusercontent.com/31KYRAN31/kyran31/main/kyranbypass.lua').content
-if not API then
-gg.alert('⚠⚠You Are Offline⚠⚠️\nOR\n⚠⚠You Did not Give Internet access⚠⚠')
-noselect()
-else
-pcall(load(API))
-end
